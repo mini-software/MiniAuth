44b1be47533a49a5b1cc0ca039198144b0a288a4
Wei Lin <shps951002@gmail.com>
2024-03-10 14:07:51 +0800

    To WebHashHistory

--- src/Frontend/src/router/index.ts
+++ src/Frontend/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 const routes = [
   {
@@ -36,7 +36,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     return savedPosition ?? { left: 0, top: 0 }
