2731714c18e0028b82a5b7a2cefd66786adc552c
Wei Lin <shps951002@gmail.com>
2024-06-09 15:20:37 +0800

    Test: Identity API Endpoints

---
+++ tests/IdentityAPIEndpoints/IdentityAPIEndpoints/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace IdentityAPIEndpoints
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddDbContext<IdentityDbContext>(options =>
+                options.UseSqlite("Data Source=identity.db"));
+            builder.Services
+                .AddIdentityApiEndpoints<IdentityUser>()
+                .AddEntityFrameworkStores<IdentityDbContext>();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            using (var scope = builder.Services.BuildServiceProvider().CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<IdentityDbContext>();
+                db.Database.Migrate();
+            }
+
+
+            var app = builder.Build();
+            app.MapGet("/", () => "Hello World!");
+            app.MapGroup("/account").MapIdentityApi<IdentityUser>();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization(); 
+            app.Run();
+        }
+    }
+}
